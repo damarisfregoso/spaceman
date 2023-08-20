a99ae0e1ac9267ddfa5edeb0346ec3287f031a31
damarisfreg <fregosodmar24@gmail.com>
2023-08-19 23:49:18 -0700

    initialize and rendering functions

--- js/main.js
+++ js/main.js
@@ -1,22 +1,59 @@
 	/*----- constants -----*/
-const secretWords = ['Spaceman', 'Stars', 'Galaxy', 'In a world far far away', 'Aliens', 'MilkyWay', 'Shooting Stars', 'Astronaut', 'Big Dipper'];
-
-
+const secretWords = ['SPACEMAN', 'STARS', 'GALAXY', 'ALIENS', 'MILKYWAY', 'SPACESHIP', 'ASTRONAUT', 'PLANETS'];
+const maxGuesses = 6;
 /*----- state variables -----*/
 let winner;
-let results;
+let word;
+let lettersGuessed;
 
-  
-	/*----- cached elements  -----*/
-  const bgPlayer = document.getElementById('bg-player');
-  const bgCheckbox = document.querySelector('input[type="checkbox"]');
-  bgPlayer.volume = .1;
-  
+/*----- cached elements  -----*/
+const bgPlayer = document.getElementById('bg-player');
+const bgCheckbox = document.querySelector('input[type="checkbox"]');
+bgPlayer.volume = .1;
+const btnEls = [...document.querySelectorAll('#btns-container > button')];
+// const imgPath = `imgs/spaceman-0${wrongGuesses.length}`;
+const msgEl = document.querySelector('h1');
+const playAgainBtn = document.querySelector('button');
+const letters = document.querySelectorAll('#letters > div')
 
-	/*----- event listeners -----*/
-  bgCheckbox.addEventListener('change', renderMusic);
+/*----- event listeners -----*/
+bgCheckbox.addEventListener('change', renderMusic);
+document.getElementById('btns-container').addEventListener('click', handleBtnClick);
+// document.getElementById('letters').addEventListener('click', handleLetter);
+playAgainBtn.addEventListener('click', init);
+/*----- functions -----*/
+init();
 
-	/*----- functions -----*/
-  function renderMusic() {
-    bgCheckbox.checked ? bgPlayer.play() : bgPlayer.pause();
-  }
+function init() {
+  winner = null;
+  render();
+}
+
+
+function render() {
+  renderMusic();
+  renderControls();
+
+}
+
+function renderControls() {
+  playAgainBtn.style.visibility = winner ? 'visible' : 'hidden';
+}
+
+function selectWord() {
+word = Math.floor(Math.random() * secretWords.length);
+console.log(word);
+}
+selectWord();
+
+function renderMusic() {
+  bgCheckbox.checked ? bgPlayer.play() : bgPlayer.pause();
+}
+
+function handleBtnClick(evt) {
+  const btn = evt.target;
+  // Ensure that a button was clicked
+  if (!btnEls.includes(btn)) return;
+  curFrame = parseInt(btn.textContent);
+  render();
+}
