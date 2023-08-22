bdc276a8e64aea8c070905d8ffa1bfa4d438f18a
damarisfreg <fregosodmar24@gmail.com>
2023-08-22 16:07:33 -0700

    spaceman moving

---
+++ README.md
@@ -0,0 +1,16 @@
+# Spaceman
+###### Created by Damaris Fregoso
+
+![](imgs/coverphoto.jpeg)
+
+[Click To play](https://github.com/damarisfregoso/spaceman)
+
+### 💻 Description 
+
+A **space theme letter guessing game**! With space theme words to challenge the player must guess the letters correctly. 
+
+### 🕹 How to Win
+ You are opted  with **_6 guesses_**  to start.  The are multipe words that will be randomly generated.  
+ 
+ The spaceman completely disappears and is aducted outspace if you cannot complete the game before gu!  Try to guess the word in 6 or less to win the game.  
+
--- js/main.js
+++ js/main.js
@@ -43,7 +43,7 @@ function render() {
   renderButtons();
   renderGuessesLeft();
   renderMesssage();
-  const imgPath = `imgs/spaceman-${wrongGuessed.length}.jpg`;
+  const imgPath = `imgs/spaceman-${wrongGuessed.length}.png`;
   imgEl.src = imgPath;
   wordDisplay.innerHTML = guess;
 }
@@ -76,11 +76,11 @@ function renderButtons() {
   letterBtns.forEach(function(btn) {
     letter = btn.innerHTML;
     if (guess.includes(letter)) {
-      btn.style.backgroundColor = 'black';
-      btn.style.color = 'rgb(131, 255, 134)'
+      btn.style.backgroundColor = 'green';
+      // btn.style.color = 'rgb(131, 255, 134)'
     } else if (wrongGuessed.includes(letter)) {
-      btn.style.backgroundColor = 'black';
-      btn.style.color = 'rgb(255, 131, 131)'
+      btn.style.backgroundColor = 'red';
+      // btn.style.color = 'rgb(255, 131, 131)'
     } else {
       //resets for play again 
       btn.style.backgroundColor = 'rgb(199, 199, 255)';
